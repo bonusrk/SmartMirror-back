df07a8cb849f4af78a851c58ac5e112e806026fd
bonusrk <word0bearer@gmail.com>
2018-07-02 14:44:33 +0300

    CORS and endpoint for tasks

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-from bottle import Bottle, run, get, post
+from bottle import Bottle, run, get, route, response, request
 from googleapiclient import discovery
 from oauth2client import file, client, tools
 from httplib2 import Http
 from pprint import pprint
 import datetime
-
-session_opts = {
-    'session.type': 'file',
-    'session.data_dir': './session/',
-    'session.auto': True,
-}
+import json
 
 app = Bottle()
 CLIENT_SECRETS_FILE = './client_secret.json'
@@ -20,37 +15,40 @@ creds = store.get()
 if not creds or creds.invalid:
     flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
-    # auth_url, _ = flow.authorization_url(prompt='consent')
-    # print('Please go to this URL: {}'.format(auth_url))
-    # code = input('Enter the authorization code: ')
-    # flow.fetch_token(code=code)
 
 service = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
 
-@get('/')
+def cors(func):
+    def wrapper(*args, **kwargs):
+        response.set_header("Access-Control-Allow-Origin", "*")
+        response.set_header("Access-Control-Allow-Methods", "GET, OPTIONS")
+        response.set_header("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+        # skip the function if it is not needed
+        if request.method == 'OPTIONS':
+            return
+
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@route('/')
 def index():
     return 'Hello world'
 
 
-@get('/calendar')
-def calendar():
+@route('/tasks')
+@cors
+def tasks():
     now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='fp8pfobln6ppgr53nqa3pp08o8@group.calendar.google.com',
                                           timeMin=now,
                                           maxResults=10, singleEvents=True,
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
-
-    if not events:
-        return 'No upcoming events found.'
-    for event in events:
-        pprint(event)
-        start = event['start'].get('dateTime', event['start'].get('date'))
-        print(start, event['summary'])
-
-    return events[0]['summary']
+    return json.dumps(events)
 
 
-run(debug=True, host='localhost', port=3000, reloader=True)
+run(debug=True, host='127.0.0.1', port=3000, reloader=True)
